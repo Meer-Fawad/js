75be6dc711c36ac0122097bf11ed0192cb086df1
Meer Fawad Ali <meerfawad65@gmail.com>
2025-09-27 08:59:09 -0700

    Add files via upload

---
+++ lec#1.js
@@ -0,0 +1,11 @@
+console.log("hello");
+for (let i =0 ;  i<10; i++){
+    console.log(i);
+
+if (i<5){
+    console.log("number is les then 5")
+}
+else{
+    console.log("number is grater then 5");
+}
+}
